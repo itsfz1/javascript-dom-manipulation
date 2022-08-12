4141e4ff8e5e1709feb3ed926f076381f1c73a12
itsfz1 <programmingway1@gmail.com>
2022-08-12 20:11:59 +0500

    Required files created

---
+++ index.js
@@ -0,0 +1,5 @@
+/**
+ * Do your manipulations here according to the instructions given in html file.
+ * Apart from that create any type of element of your choice and append it to the body with a text "success".
+ * Use querySelector and querySelectorAll to find the element you want to manipulate.
+ */
